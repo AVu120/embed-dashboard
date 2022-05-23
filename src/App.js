8770e89d4a3955584b675780930c82d88ddd8b72
Anthony Hien Vu <anthony.vu@checkbox.ai>
2022-05-23 17:34:20 +1000

    Save.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Embedded dashboard demo below.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Dashboard URL: */}
+        {/* https://localhost:8443/app/team/4/dashboard */}
+        {/* Embedded Dashboard URL : */}
+        {/* https://localhost:8443/app/team/4/dashboard?isEmbedded=true */}
+        {/* App URL: */}
+        {/* https://localhost:8443 */}
+        {/* Login URL: */}
+        {/* https://localhost:8443/login */}
+        {/* Deployed URL: */} 
+        {/* https://dev-9375-embed-dashboard-in-iframe.checkbox.ninja */}
+        {/* Deployed Dashboard URL: */} 
+        {/* https://dev-9375-embed-dashboard-in-iframe.checkbox.ninja/app/team/2/dashboard */}
+        {/* Embedded Deployed Dashboard URL: */} 
+        {/* https://dev-9375-embed-dashboard-in-iframe.checkbox.ninja/app/team/2/dashboard?isEmbedded=true */}
+        <iframe loading="lazy" width="1140" height="541.25" src="https://dev-9375-embed-dashboard-in-iframe.checkbox.ninja/app/team/2/dashboard?isEmbedded=true" frameborder="0" allowfullscreen="true"></iframe>
       </header>
     </div>
   );
